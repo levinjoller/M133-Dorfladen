48d1060660a09e9ae536aca1a63d2a82809260fd
Levinuk <levin_joller@sluz.ch>
2019-12-13 23:20:58 +0100

    Added session functionality, installed required dependencies

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import express from 'express';
-
+import expressSession from 'express-session';
 const app = express();
+const port = 8080;
+
+app.use(expressSession({
+    secret: "I'%m23-o&uT",
+    resave: false,
+    saveUninitialized: true,
+}));
 
 app.get("/", (req,res) => {
     res.sendStatus(200);
 });
 
-app.listen(8080, () => {
-    console.log("runs on port 8080");
+app.listen(port, () => {
+    console.log(`server started: ${port}`);
 });
